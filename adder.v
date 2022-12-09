b171a3a6586ae46788d7d17d00800e15af221510
Ben Manicke <manicb@rpi.edu>
2022-12-09 16:35:24 -0500

    Added ALU
    Added ALU and made a spacing change to adder.v

--- adder.v
+++ adder.v
@@ -4,6 +4,6 @@ module adder32(result, a, b);
    input[31:0] b;
    output [31:0] result;
    wire [32:0] sum = {1'b0,a} + {1'b0,b};
-   assign      result = sum[32] ? sum[32:1]: sum[31:0];
+   assign result = sum[32] ? sum[32:1]: sum[31:0];
 
 endmodule
